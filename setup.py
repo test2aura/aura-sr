04cb6d2a45263fc4accc0e68d311adda7cf2d4e0
test2aura <test2aura@gmail.com>
2024-08-09 00:28:25 +0300

    Update setup.py
    typo fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="aura-sr-custom",
     version="0.0.4",
     description="Custom GAN-based Super-Resolution for AI generated images, based on the GigaGAN architecture.",
-    py_modules=["aura_sr_custom"]
+    py_modules=["aura_sr_custom"],
     install_requires=[
         "torch>=2.0",
         "torchvision",
